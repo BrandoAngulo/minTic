2f6cc69b527664f37ab1ee05a6abf235d87f18b3
BrandoAngulo <steven.sm.1992@gmai.com>
2022-06-11 14:17:26 -0500

    test 1

---
+++ Test1/tienda.py
@@ -0,0 +1,13 @@
+codPro = int(input());
+namePro = str(input());
+cantidad = float(input());
+valUndPro = float(input());
+iva = 0.19;
+# valor producto sin iva
+valpro = (valUndPro * cantidad);
+# valor producto mas iva
+proFinal = (valpro * iva) + valpro;
+
+mensaje = (f'Codigo: {codPro}\nNombre: {namePro} \nValour unitario sin iva: {valpro} \nPrecio con iva: ${proFinal}');
+print(mensaje);
+
