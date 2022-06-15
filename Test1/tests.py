a21bdf927db0a48f3dfc140b66a190909acc431d
BrandoAngulo <steven.sm.1992@gmai.com>
2022-06-15 17:45:57 -0500

    test2 resuelto pero mejorar

--- Test1/tests.py
+++ Test1/tests.py
@@ -14,46 +14,22 @@ for i in range(n):
     if typeIva == 3: 
         totalProducts = (costo + iva)*cantBuy;
         totalIva = iva*cantBuy;
+        #total = priceFinalProduct * cantBuy;
+        print(f'{code}\n{name}\n{priceTsin}\n{totalProducts}');
+
     elif typeIva == 2:
         totalProducts = (costo + assets)*cantBuy;
         totalIva = assets * cantBuy;
+        #total = priceFinalProduct * cantBuy;
+        print(f'{code}\n{name}\n{priceTsin}\n{totalProducts}');
+
     elif typeIva == 1:
         totalProducts = priceTsin;
         total = priceTsin *cantBuy;
+        print(f'{code}\n{name}\n{priceTsin}\n{totalProducts}');
+
         excento = False;
     if  excento == True:
         sumaIva += totalIva;
-    print(f'{code}\n{name}\n{priceTsin}\n{totalProducts}');
     sumaProduct += totalProducts ;
 print(f'{sumaProduct}\n{sumaIva}');
-
-
-""" 
-COMO DEBIA HABER RESPONDIDO SIN TANTO CODIGO REPETIDO
-N=int(input())
-total_ventas=0
-total_iva=0
-for i in range(N):
-    codigo=int(input())
-    nombre=input()
-    cantidad=float(input())
-    valor_unitario=float(input())
-    tipo=int(input())
-    valor_producto=cantidad*valor_unitario
-    if tipo==1:
-        iva=0
-    elif tipo==2:
-        iva=valor_producto*0.05
-    else:
-        iva=valor_producto*0.19
-    valor_final=valor_producto+iva
-    total_iva+=iva
-    total_ventas+=valor_final
-    print(codigo)
-    print(nombre)
-    print(valor_producto)
-    print(valor_final)
-print(total_ventas)
-print(total_iva)
-
- """
