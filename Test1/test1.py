dff983d17163e3a8351a8d4459b8a367c7d33a1c
BrandoAngulo <steven.sm.1992@gmail.com>
2022-06-15 20:34:59 -0500

    codigo del test2  mejorado

--- Test1/test1.py
+++ Test1/test1.py
@@ -3,11 +3,8 @@ namePro = str(input());
 cantidad = float(input());
 valUndPro = float(input());
 iva = 0.19;
-# valor producto sin iva
 valpro = (valUndPro * cantidad);
-# valor producto mas iva
 proFinal = (valpro * iva) + valpro;
-
 mensaje = (f'Codigo: {codPro}\nNombre: {namePro} \nPrecio unitario sin iva: {valpro} \nPrecio con iva: ${proFinal}');
 print(mensaje);
     
